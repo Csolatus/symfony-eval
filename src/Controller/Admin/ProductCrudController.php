36b59cf8a88b5ebeaf0e84bbb01e48c7886795c8
Csolatus <lucas.lanoue@efrei.net>
2025-12-16 18:43:18 +0100

    Modification du gitignore et modification de l'ajout de produit pour l'admin

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -4,7 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,14 +18,16 @@ class ProductCrudController extends AbstractCrudController
         return Product::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('name'),
+            SlugField::new('slug')->setTargetFieldName('name'),
+            MoneyField::new('price')->setCurrency('EUR')->setStoredAsCents(false),
             TextEditorField::new('description'),
+            TextField::new('imageName'),
+            AssociationField::new('category'),
         ];
     }
-    */
 }
